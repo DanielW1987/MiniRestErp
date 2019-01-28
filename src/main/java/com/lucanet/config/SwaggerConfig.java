1973924cc681489360e1811ca16108001d857dc8
DanielW <DanielW@lucanet.com>
2019-01-28 13:08:49 +0100

    log swagger ui url as INFO on console

--- src/main/java/com/lucanet/config/SwaggerConfig.java
+++ src/main/java/com/lucanet/config/SwaggerConfig.java
@@ -1,21 +1,33 @@
 package com.lucanet.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.service.Contact;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.annotation.PostConstruct;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
     private static final Contact DEFAULT_CONTACT = new Contact("Daniel Wagner", "http://lucanet.com/", "danielw@lucanet.com");
+    private              Logger  LOG             = LoggerFactory.getLogger(SwaggerConfig.class.getName());
+
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+
+    @Value("${server.port}")
+    private String serverPort;
 
     private static final ApiInfo DEFAULT_API_INFO = new ApiInfoBuilder()
             .title("Mini-REST-ERP")
@@ -34,6 +46,12 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
+    }
+
+    @PostConstruct
+    private void init() {
+        String swaggerUiUrl = "http://localhost:" + serverPort + contextPath + "/swagger-ui.html";
+        LOG.info("Swagger UI: " + swaggerUiUrl);
     }
 
 }
